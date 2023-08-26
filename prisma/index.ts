77f951554f7915a823f8cfa041d9e9e66a935025
Anupam kumar <anupamkumar740@gmail.com>
2023-08-26 13:38:26 +0530

    min change

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,15 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
 declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
+  var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === "production") {
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
